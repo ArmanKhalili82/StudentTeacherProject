6702ab26e3fd79615d64fdbd2a4ed82dabe7b587
ArmanKhalili82 <hykhalili@yahoo.com>
2023-12-16 21:47:14 +0100

    program

--- StudentTeacherProject/Program.cs
+++ StudentTeacherProject/Program.cs
@@ -1,33 +1,20 @@
 ﻿using StudentTeacherProject;
 
-var db = new ApplicationDbContext();
+Console.WriteLine("Enter Your Student Or Teacher:");
+var command = Console.ReadLine();
 
-Console.WriteLine("Enter Your Student Name:");
-var studnetname = Console.ReadLine();
+if (command == "Student")
+{
+    while (true)
+    {
+        Console.WriteLine("Enter Your Student:");
+        var student = Console.ReadLine();
+        Console.WriteLine("Enter Your Family");
+        var family = Console.ReadLine();
+    }
+}
 
-Console.WriteLine("Enter Your Student Family:");
-var studnetfamily = Console.ReadLine();
+if (command == "Teacher")
+{
 
-Console.WriteLine("Enter Your Score");
-var score = int.Parse(Console.ReadLine());
-
-var student = new Student();
-student.ShowName();
-student.ShowName();
-student.ShowName();
-var st = db.Persons.Where(s => s.Name == studnetname).FirstOrDefault();
-db.Students.Add((Student)st);
-db.SaveChanges();
-
-Console.WriteLine("Enter Your Teacher:");
-var teachername = Console.ReadLine();
-
-Console.WriteLine("Enter Your Course:");
-var course = Console.ReadLine();
-
-var teacher = new Teacher();
-teacher.ShowName();
-teacher.ShowName();
-var te = db.Persons.Where(t => t.Name == teachername).FirstOrDefault();
-db.Teachers.Add((Teacher)te);
-db.SaveChanges();
+}
